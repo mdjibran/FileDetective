c569244f935c29a8119bdc8f09ec9f6c6ef48d6e
mdjibran <mohdjibran316@gmail.com>
2017-02-03 11:50:15 -0500

    File Detective Initial Push

---
+++ advanced.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FileDetective
+{
+    partial class advanced
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this._tbextension = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this._tbfileName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _tbextension
+            // 
+            this._tbextension.Location = new System.Drawing.Point(43, 50);
+            this._tbextension.Name = "_tbextension";
+            this._tbextension.Size = new System.Drawing.Size(177, 20);
+            this._tbextension.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 78);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(124, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Search By File Name";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(142, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Search By File Extension";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // _tbfileName
+            // 
+            this._tbfileName.Location = new System.Drawing.Point(43, 101);
+            this._tbfileName.Name = "_tbfileName";
+            this._tbfileName.Size = new System.Drawing.Size(177, 20);
+            this._tbfileName.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 150);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selection";
+            // 
+            // advanced
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 172);
+            this.Controls.Add(this._tbfileName);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this._tbextension);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "advanced";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "advanced";
+            this.Load += new System.EventHandler(this.advanced_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox _tbextension;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox _tbfileName;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ advanced.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileDetective
+{
+    public partial class advanced : Form
+    {
+        public advanced()
+        {
+            InitializeComponent();
+        }
+
+        private void advanced_Load(object sender, EventArgs e)
+        {
+            _tbextension.Enabled = false;
+            _tbfileName.Enabled = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            _tbfileName.Text = "";
+            _tbfileName.Enabled = false;
+            _tbextension.Enabled = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            _tbextension.Text = "";
+            _tbextension.Enabled = false;
+            _tbfileName.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selection = "";
+            if (_tbfileName.Text != "")
+                selection = _tbfileName.Text.Trim();
+
+            else if (_tbextension.Text != "")
+                selection = _tbextension.Text.Trim();
+
+            else
+                MessageBox.Show("Please enter file extension or file name to search", "FileDetective");
+
+            Form1 obj = new Form1();
+            obj.getFromAdvanceForm(selection);
+            if(selection != "")
+            this.Close();
+        }
+    }
+}
